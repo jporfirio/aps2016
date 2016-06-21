96fb036e609643a73a7326a341b94c889e32c6df
Felipe <Felipe@Felipe>
2016-06-20 23:53:14 -0300

    Commit 20.06

--- src/src/seega/view/AtorJogador.java
+++ src/src/seega/view/AtorJogador.java
@@ -30,6 +30,7 @@ public class AtorJogador {
         if (!conectado) {
             String servidor = this.obterDadosConexao();
             boolean exito = rede.conectar(servidor, idUsuario);
+            
             if (exito) {
                 tabuleiro.estabelecerConectado(true);
                 return 0;
